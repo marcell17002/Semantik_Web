254df9cb38f17ba80a7b6c95e326f46625979f6e
Facetozvers <whatthefapmc@gmail.com>
2020-06-08 19:04:51 +0700

    fix css landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,7 +10,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/home.css') }}" />
     </head>
     <body>
         @include('includes.header')
