c3cc02859e64113ae4ec1211df85c1332f39af68
Facetozvers <whatthefapmc@gmail.com>
2020-06-08 19:00:58 +0700

    css fix

--- resources/views/article-pick.blade.php
+++ resources/views/article-pick.blade.php
@@ -12,7 +12,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/article-pick.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/article-pick.css') }}" />
     </head>
     <body>
         @include('includes/header')
